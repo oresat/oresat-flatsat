2d08ca938bb1bb92ff55b5befcf11fa2b64affc9
ryanpdx <rmedick@protonmail.com>
2023-06-24 19:18:41 -0700

    add prints for debugging to cfc only firmware

--- c3-surrogate/trinket-firmware/cfc-only/code.py
+++ c3-surrogate/trinket-firmware/cfc-only/code.py
@@ -23,22 +23,36 @@ cfc_sensor_card = 0x1E
 result = bytearray(1)
 
 try:
+    print('hi')
+    time.sleep(1)
+
+    print('power on processor card')
     i2c.writeto(cfc_card, bytes([3,0b10000]))
     time.sleep(.1)
     i2c.writeto(cfc_card, bytes([1,0b1000]))
     i2c.writeto_then_readfrom(cfc_card, bytes([1]), result)
+
+    print('sleep')
+    time.sleep(1)
+
+    print('power on sensor card')
     i2c.writeto(cfc_sensor_card, bytes([3,0b10000]))
     time.sleep(.1)
     i2c.writeto(cfc_sensor_card, bytes([1,0b1000]))
     i2c.writeto_then_readfrom(cfc_sensor_card, bytes([1]), result)
-    while 1:
-        continue
 
+
+    while 1:
+        print('loop')
+        time.sleep(1)
 finally:
+    print('finally')
     i2c.writeto(cfc_sensor_card, bytes([1,0b0]))
     i2c.writeto_then_readfrom(cfc_sensor_card, bytes([1]), result)
     print(hex(result[0]))
+
     i2c.writeto(cfc_card, bytes([1,0b0]))
     i2c.writeto_then_readfrom(cfc_card, bytes([1]), result)
     print(hex(result[0]))
+
     i2c.unlock()
